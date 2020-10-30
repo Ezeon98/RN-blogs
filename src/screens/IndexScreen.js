ef9524296b836b5acd8f0388fedd919f36839e93
Ezeon98 <ezeon3@gmail.com>
2020-10-30 13:04:19 -0300

    1.0
    1.0

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native'
+
+const IndexScreen = () => {
+    return(
+        <Text>Index Screen</Text>
+    )
+}
+
+styles=StyleSheet.create({
+
+})
+
+export default IndexScreen;
